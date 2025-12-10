1f58c6fc7088126c24d85b63baa51047e08b3412
ddjlb7598 <q18342117598@163.com>
2025-12-10 13:36:22 +0800

    Delete 卡密系统7.lua

--- 卡密系统7.lua
+++
@@ -1,2 +0,0 @@
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/ddjlb7598/-0888/refs/heads/main/%E8%BF%AA%E8%84%9A%E6%9C%AC%E8%87%AA%E5%88%B6.lua"))()
-            Notification:Warning("注意", "检测到您已长时间未操作", 10)
